fadf2415aed1abb535b98ce554a2644c720a0931
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-02-28 17:33:47 -0500

    Write products router and products slice

---
+++ client/features/products/productsSlice.js
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// GET /api/products
+export const fetchAllProducts = createAsyncThunk(
+  "fetch all products",
+  async () => {
+    try {
+      const { data } = await axios.get("/api/products");
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+// POST /api/products
+export const addProduct = createAsyncThunk("add product", async () => {
+  try {
+    const { data } = await axios.post("/api/products");
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// DELETE /api/products
+export const deleteProduct = createAsyncThunk("delete product", async (id) => {
+  try {
+    const { data } = await axios.delete(`/api/products/${id}`);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(addProduct.fulfilled, (state, action) => {
+        state.push(action.payload);
+      })
+      .addCase(deleteProduct.fulfilled, (state, action) => {
+        return [];
+      });
+  },
+});
+
+export const selectProducts = (state) => {
+  return state.products;
+};
+
+export default productsSlice.reducer;
