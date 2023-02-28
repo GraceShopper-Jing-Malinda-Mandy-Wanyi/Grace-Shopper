5054629049b956cbb1ccb22051d4b4c63269d028
Jing Mo <Jingmo39@gmail.com>
2023-02-28 14:54:44 -0500

    userSlice

--- client/app/store.js
+++ client/app/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from '../features/auth/authSlice';
+import usersSlice from '../features/User/usersSlice';
 
 const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { 
+    auth: authReducer,
+    users: usersSlice,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
