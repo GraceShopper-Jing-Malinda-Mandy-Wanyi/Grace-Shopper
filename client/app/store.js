62b27aeab2df72a758c4b57a22bf358b0e0b88a1
Malinda Singh <malinda.singh@gmail.com>
2023-03-01 16:30:16 -0500

    updated the store file

--- client/app/store.js
+++ client/app/store.js
@@ -4,13 +4,18 @@ import authReducer from '../features/auth/authSlice';
 import usersSlice from '../features/User/usersSlice';
 import singleUserSlice from '../features/User/singleUserSlice';
 import productsSlice from '../features/products/productsSlice';
+import singleOrderSlice from '../features/Orders/singleOrderSlice';
+import allOrders from "../features/Orders/AllOrders";
+import allOrdersSlice from '../features/Orders/allOrdersSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     users: usersSlice,
     singleUser: singleUserSlice,
-    products: productsSlice
+    products: productsSlice,
+    singleOrder: singleOrderSlice,
+    allOrders: allOrdersSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
