83b64a1f156b2d42ce7dfc81340d4f545fa25e02
Jing Mo <Jingmo39@gmail.com>
2023-03-01 10:27:27 -0500

    updated user router

--- client/features/User/singleUserSlice.js
+++ client/features/User/singleUserSlice.js
@@ -14,6 +14,44 @@ export const fetchSingleUserSlice = createAsyncThunk(
         }
     }
 );
+export const deleteUserAsync = createAsyncThunk(
+    "deleteUser",
+    async (id) => {
+      try {
+        const { data } = await axios.delete(`/api/users/${id}`);
+        console.log(data);
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  );
+  export const editUserAsync = createAsyncThunk(
+    "editUser",
+    async ({ id, firstName, lastName, email, username, password }) => {
+      const { data } = await axios.put(`/api/users/${id}`, {
+        id,
+        firstName,
+        lastName,
+        email,
+        username,
+        password,
+      });
+      return data;
+    }
+  );
+  
+  export const unregisterUserAsync = createAsyncThunk(
+    "unregisterUser",
+    async (id) => {
+        const { data } = await axios.put(`/api/users/${id}/unregister`, {
+          // campusId: null,
+        });
+        console.log(data);
+        return data;
+    
+      }
+  );
 
 const singleUserSlice = createSlice({
     name: 'singleUser',
@@ -23,6 +61,15 @@ const singleUserSlice = createSlice({
         builder.addCase(fetchSingleUserSlice.fulfilled, (state, action) => {
             return action.payload;
         });
+        builder.addCase(deleteUserAsync.fulfilled, (state, action) => {
+            return {};
+          });
+          builder.addCase(editUserAsync.fulfilled, (state, action) => {
+            return action.payload;
+          });
+          builder.addCase(unregisterUserAsync.fulfilled, (state, action) => {
+            return action.payload;
+          });
     },
 });
 
