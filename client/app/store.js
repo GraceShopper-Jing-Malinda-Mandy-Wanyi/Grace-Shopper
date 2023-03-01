5c9f28cb0d0d3a13511b03ff4e542245d7f4a601
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-03-01 15:45:16 -0500

    Write SingleProduct component and slice

--- client/app/store.js
+++ client/app/store.js
@@ -1,19 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import authReducer from '../features/auth/authSlice';
-import usersSlice from '../features/User/usersSlice';
-import singleUserSlice from '../features/User/singleUserSlice';
-import productsSlice from '../features/products/productsSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import authReducer from "../features/auth/authSlice";
+import usersSlice from "../features/User/usersSlice";
+import singleUserSlice from "../features/User/singleUserSlice";
+import productsSlice from "../features/products/productsSlice";
+import singleProductSlice from "../features/products/singleProductSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     users: usersSlice,
     singleUser: singleUserSlice,
-    products: productsSlice
+    products: productsSlice,
+    singleProduct: singleProductSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
-export * from '../features/auth/authSlice';
+export * from "../features/auth/authSlice";
