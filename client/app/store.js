0d78da6c336345ccaac7fde8778e0bdc31723ed5
Jing Mo <Jingmo39@gmail.com>
2023-03-01 16:31:58 -0500

    updated

--- client/app/store.js
+++ client/app/store.js
@@ -4,7 +4,6 @@ import authReducer from '../features/auth/authSlice';
 import usersSlice from '../features/User/usersSlice';
 import singleUserSlice from '../features/User/singleUserSlice';
 import productsSlice from '../features/products/productsSlice';
-import searchSlice from '../features/search/searchSlice';
 
 const store = configureStore({
   reducer: {
@@ -12,7 +11,7 @@ const store = configureStore({
     users: usersSlice,
     singleUser: singleUserSlice,
     products: productsSlice,
-    searchProducts: searchSlice,
+    
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
