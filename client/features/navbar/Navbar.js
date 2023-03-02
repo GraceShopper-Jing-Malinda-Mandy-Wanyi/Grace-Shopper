4308208a628f4f6aacfd64ec81429b4c0679cb58
mandy-chin <chin.mandyy@gmail.com>
2023-03-01 19:53:12 -0500

    reorg navbar

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -15,74 +15,79 @@ const Navbar = () => {
 
   return (
     <div>
-      <h1>Winey wine</h1>
       <nav>
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <button type="button" onClick={logoutAndRedirectHome}>
-              Logout
-            </button>
+        <div id="nav-container">
+          <div id="company-name">
+            <h1>Winey Wine</h1>
           </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <div>
-              
-              <div className="search-bar">
-            
-                <input type="text" placeholder="Search..."></input>
-                <button type="submit">Search</button>
-                <div className="cart-container">
-                  <button className="cart-btn">
-                    <span className="cart-icon">&#128722;</span>
+
+          <div id="search-bar-login-signup">
+            <div className="search-bar">
+              <input type="text" placeholder="Search..."></input>
+              <button type="submit">Search</button>
+            </div>
+
+            <div id="login-signup-cart">
+              {isLoggedIn ? (
+                <div className="login-logout">
+                  <Link to="/home">Home</Link>
+                  <button type="button" onClick={logoutAndRedirectHome}>
+                    Logout
                   </button>
                 </div>
-              </div>
-            </div>
-            <div className="drops">
-            <div className="dropdown">
-                <button className="dropbtn">All Products</button>
-               
-              </div>
-              <div className="dropdown">
-                <button className="dropbtn">Wine</button>
-                <div className="dropdown-content">
-                  <a href="#">Link 1</a>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
+              ) : (
+                <div className="login-logout">
+                  <Link to="/login">Login</Link>
+                  <Link to="/signup">Sign Up</Link>
                 </div>
-              </div>
-              <div className="dropdown">
-                <button className="dropbtn">Beer</button>
-                <div className="dropdown-content">
-                  <a href="#">Link 1</a>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
-                </div>
-              </div>
-              <div className="dropdown">
-                <button className="dropbtn">Spirit</button>
-                <div className="dropdown-content">
-                  <a href="#">Link 1</a>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
-                </div>
-              </div>
-              <div className="dropdown">
-                <button className="dropbtn">Size</button>
-                <div className="dropdown-content">
-                  <a href="#">Link 1</a>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
-                </div>
+              )}
+
+              <div className="cart-container">
+                <button className="cart-btn">
+                  <span className="cart-icon">&#128722;</span>
+                </button>
               </div>
             </div>
           </div>
-        )}
+
+          <div className="drops">
+            <div className="dropdown">
+              <button className="dropbtn">All Products</button>
+            </div>
+            <div className="dropdown">
+              <button className="dropbtn">Wine</button>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+            <div className="dropdown">
+              <button className="dropbtn">Beer</button>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+            <div className="dropdown">
+              <button className="dropbtn">Spirit</button>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+            <div className="dropdown">
+              <button className="dropbtn">Size</button>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
       <hr />
     </div>
