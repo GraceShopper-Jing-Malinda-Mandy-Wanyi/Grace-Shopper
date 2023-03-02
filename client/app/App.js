5027634f23265cb4c4d5479f42f3ce1fdb6e8f72
mandy-chin <chin.mandyy@gmail.com>
2023-03-02 17:11:24 -0500

    fixed login infinite loop

--- client/app/App.js
+++ client/app/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { me } from './store';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../features/navbar/Navbar';
 import AppRoutes from './AppRoutes';
 
 const App = () => {
+  const user = useSelector(state => state.auth)
   const dispatch = useDispatch()
 
   //checks to see if you have a token in local storage -> if you do, the implication is that you logged in without logging out -> this will log you back in
-  if(window.localStorage.getItem("token")){
+  if(window.localStorage.getItem("token") && Object.keys(user.me).length === 0){
     dispatch(me())
   }
 
