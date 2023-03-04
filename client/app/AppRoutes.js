70d1fe2a234f1857dce53670bdc306efd58fed42
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-03-04 16:26:11 -0500

    Write Route to Account component

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -6,9 +6,10 @@ import Home from '../features/home/Home';
 import { me } from './store';
 import SingleProduct from '../features/products/SingleProduct';
 import Products from '../features/products/Products';
-import SingleUser from '../features/User/SingleUser';
-import SingleOrder from '../features/Orders/SingleOrder'
+// import SingleUser from '../features/User/SingleUser';
+// import SingleOrder from '../features/Orders/SingleOrder'
 import Cart from "../features/cart/Cart";
+import Account from '../features/User/Account';
 /**
  * COMPONENT
  */
@@ -17,6 +18,8 @@ const AppRoutes = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const url = useLocation();
   const navigate = useNavigate();
+
+  const userId = useSelector((state) => state.auth.me.id);
 
   useEffect(() => {
     if (url.pathname === "/") {
@@ -36,6 +39,8 @@ const AppRoutes = () => {
 
         <Route path="/landing" element={<Home />} />
 
+        <Route path={`/account/${userId}`} element={<Account/>}/>
+
         {/* <Route
             path="/login"
             element={<AuthForm name="login" displayName="Login" />}
@@ -49,9 +54,9 @@ const AppRoutes = () => {
         <Route path="/products/" element={<Products />} />
 
 
-        <Route path="/users/:userId/orders/:orderid" element={<SingleOrder />} />
+        {/* <Route path="/users/:userId/orders/:orderid" element={<SingleOrder />} />
 
-        <Route path="/users/:userId" element={<SingleUser />} />
+        <Route path="/users/:userId" element={<SingleUser />} /> */}
 
         <Route path="/cart/" element={<Cart />} />
 
