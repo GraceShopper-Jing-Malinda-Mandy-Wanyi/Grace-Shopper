7d45e91174649c001d52ee2c16e23081fce85394
mandy-chin <chin.mandyy@gmail.com>
2023-03-06 13:03:42 -0500

    fixed admin routes

--- server/api/users.js
+++ server/api/users.js
@@ -8,6 +8,7 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
+      order: [["id", "ASC"]],
       attributes: ['id', 'username', 'firstName', 'lastName', 'userType']
     })
     res.json(users)
