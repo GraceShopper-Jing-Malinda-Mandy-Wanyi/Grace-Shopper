910c2910b87899178a0fdd3c3e88059a48fda0c2
mandy-chin <chin.mandyy@gmail.com>
2023-03-07 09:29:14 -0500

    linked shop now button on landing page to all products

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { fetchAllProducts, selectProducts } from "../products/productsSlice";
 import {
-    fetchAllProducts,
-    selectProducts,
-  } from "../products/productsSlice";
-  import { fetchRandomProduct, selectRandomProduct} from "../products/singleProductSlice";
-  import { useDispatch } from "react-redux";
-  import { useEffect } from "react";
-  import { addCartItemAsync } from "../cart/cartSlice";
+  fetchRandomProduct,
+  selectRandomProduct,
+} from "../products/singleProductSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { addCartItemAsync } from "../cart/cartSlice";
+import { Link } from "react-router-dom";
 
 /**
  * COMPONENT
@@ -21,7 +22,6 @@ const Home = (props) => {
   useEffect(() => {
     dispatch(fetchAllProducts());
     dispatch(fetchRandomProduct());
-
   }, [dispatch]);
 
   const addToCart = (event) => {
@@ -32,39 +32,47 @@ const Home = (props) => {
     };
     dispatch(addCartItemAsync(cartItem));
   };
+
+  let username = me.username;
+  if (me.username) {
+    username = me.username.slice(0,1).toUpperCase() + me.username.slice(1);
+  }
   return (
     <div>
-          <div>
-      <h3>Welcome, {me.username}</h3>
-
       <div>
-        <p>Find the perfect drink for any occasion</p>
-        <button>Shop Now</button>
+        <h3>Welcome, {username}!</h3>
+
+        <div>
+          <p>Find the perfect drink for any occasion</p>
+          <Link to="/products?type=allproducts"> <button>Shop Now</button></Link>
+        </div>
+
+        <section className="featured-products">
+          <h2>Featured Products</h2>
+          <ul>
+            {randomProduct && randomProduct.length > 0 ? (
+              randomProduct.map((product) => (
+                <li key={product.id}>
+                  <img
+                    className="landing-product-images"
+                    src={product.img}
+                    alt={product.name}
+                  />
+                  <h3>{product.name}</h3>
+                  <p>${product.price}</p>
+                  <button value={product.id} onClick={addToCart}>
+                    Add to Cart
+                  </button>
+                </li>
+              ))
+            ) : (
+              <h1>Oops, there was an error!</h1>
+            )}
+          </ul>
+        </section>
       </div>
-
-      <section className="featured-products">
-        <h2>Featured Products</h2>
-        <ul>
-        {randomProduct && randomProduct.length > 0 ? (
-          randomProduct.map((product) => (
-            <li key={product.id}>
-                <img className="landing-product-images" src={product.img} alt={product.name} />
-                <h3>{product.name}</h3>
-                <p>${product.price}</p>
-                <button value={product.id} onClick={addToCart}>Add to Cart</button>
-              </li>
-          ))
-        ) : (
-          <h1>Oops, there was an error!</h1>
-        )}
-
-        </ul>
-      </section>
-    </div>
-
     </div>
   );
 };
 
 export default Home;
-
